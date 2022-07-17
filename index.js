4e774aff292926f6099a53e4581ee53696b900d9
Eriche Gonzales <erichelouise@gmail.com>
2022-07-17 16:41:32 -0400

    Done with lab

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let middle = 0
+  if (word.length % 2 === 0) middle = word.length / 2
+  if (word.length % 2 === 1) middle = (word.length / 2) - 0.5
+
+  for (let i = 0; i < middle; i++) {
+    let j = word.length - i - 1
+    if (word[i] !== word[j]) return false
+  }
+  return true
 }
 
-/* 
-  Add your pseudocode here
-*/
+isPalindrome('robot')
 
-/*
+/* Add your pseudocode here
+  check if the string has an even or odd length
+  if even, iterate through the half of the word length (6 / 2 = 3)
+  if odd, iterate through half of the word length rounded down (7 / 2 = 3.5 => 3)
+  then,
+    get the first element of the array and the last element of the array
+    check if they are equal => if not, return false
+    loop again with the second element and second to last element until the index reaches the middle of the array
+    if loop goes through specified elements => return true
+  madam
+  middle = 3, d
+  word[1] = m
+  word[2] = a
+  word[3] = d
+  word[5] = a
+  word[5] = m
+*/ 
+
+/* 
   Add written explanation of your solution here
 */
 
@@ -19,7 +44,22 @@ if (require.main === module) {
   console.log("");
 
   console.log("Expecting: false");
+  console.log("=>", isPalindrome("noon"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("repaper"));
+
+  console.log("");
+
+  console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("rotated"));
 }
 
 module.exports = isPalindrome;
